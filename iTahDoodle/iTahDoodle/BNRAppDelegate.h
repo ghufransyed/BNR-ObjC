c03e67fc5c5a8bab7603a76e92edb66ab230e3ef
Ghufran Syed <ghufran.syed@gmail.com>
2014-08-16 21:28:35 -0400

    Initial commit Chapter 31

---
+++ iTahDoodle/iTahDoodle/BNRAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  BNRAppDelegate.h
+//  iTahDoodle
+//
+//  Created by Ghufran Syed on 8/16/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void) addTask:(id)sender;
+
+
+
+@end
---
+++ iTahDoodle/iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by Ghufran Syed on 8/16/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
